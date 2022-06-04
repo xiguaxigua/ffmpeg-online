79336d42dc63c0550e4719c5a7564f45f5b3093c
xiguaxigua <xiguaxigua888@gmail.com>
2022-06-04 12:12:23 +0800

    feat: change template

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ]
+  },
 }
+
 
 module.exports = nextConfig
