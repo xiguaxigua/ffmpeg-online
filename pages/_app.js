98a182fc65d0b9c0b912714ff798f0267fb63fb3
xiguaxigua <xiguaxigua888@gmail.com>
2023-05-28 09:12:18 +0800

    feat: change tongji

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import "antd/dist/antd.css";
 import "./app/index.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script>
+        {`
+          var _hmt = _hmt || [];
+          (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?0a1ca7fd41de7c6002f320f032408621";
+            var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+          })();
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
