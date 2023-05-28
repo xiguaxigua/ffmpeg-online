a821a497998ba5301b34d1a2380965e22a31977e
xiguaxigua <xiguaxigua888@gmail.com>
2023-05-28 09:25:37 +0800

    feat: delete tongji

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import "antd/dist/antd.css";
 import "./app/index.css";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script id="my-script" strategy="beforeInteractive" src="https://hm.baidu.com/hm.js?0a1ca7fd41de7c6002f320f032408621"></Script>
       <Component {...pageProps} />
     </>
   );
