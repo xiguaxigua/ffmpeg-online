052031f1680480e75aba5ea04d2b771daa04b171
xiguaxigua <xiguaxigua888@gmail.com>
2023-05-28 09:21:07 +0800

    feat: change script

--- pages/_app.js
+++ pages/_app.js
@@ -5,17 +5,7 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script id="my-script" strategy="beforeInteractive">
-        {`
-          var _hmt = _hmt || [];
-          (function() {
-            var hm = document.createElement("script");
-            hm.src = "https://hm.baidu.com/hm.js?0a1ca7fd41de7c6002f320f032408621";
-            var s = document.getElementsByTagName("script")[0]; 
-            s.parentNode.insertBefore(hm, s);
-          })();
-        `}
-      </Script>
+      <Script id="my-script" strategy="beforeInteractive" src="https://hm.baidu.com/hm.js?0a1ca7fd41de7c6002f320f032408621"></Script>
       <Component {...pageProps} />
     </>
   );
