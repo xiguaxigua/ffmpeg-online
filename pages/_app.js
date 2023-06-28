1a4ccf0ff77b647e85b626a94292c06c3207d42c
xiguaxigua <xiguaxigua888@gmail.com>
2023-06-28 19:23:45 +0800

    feat: add ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
 import "antd/dist/antd.css";
 import "./app/index.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-M4JFD2DM29`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-M4JFD2DM29', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
